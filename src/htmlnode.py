76476e81e696b548c982ca124102a75250029f8f
Michael-Cave <michael.cave1990@gmail.com>
2024-05-14 15:23:25 -0400

    Finished HTMLNode tests and wrote LeafNode subclass

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,3 +30,25 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+   
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        extracted = self.props_to_html()
+
+        if self.value == None:
+            raise ValueError
+        
+        if self.tag == None:
+            return f'{self.value}'
+
+        if self.props == None:
+            return f'<{self.tag}>{self.value}</{self.tag}'
+        
+        else:
+            return f'<{self.tag} {extracted}>{self.value}</{self.tag}>'
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,5 +2,13 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html(self):
-        
+    def test_correct_formatting(self):
+        node = HTMLNode(props={"href": "https://boot.dev", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), 'href="https://boot.dev" target="_blank"')
+
+    def test_empty_dictionary(self):
+        node = HTMLNode(props={})
+        self.assertEqual(node.props_to_html(), "")
+
+if __name__ == "__main__":
+    unittest.main()
